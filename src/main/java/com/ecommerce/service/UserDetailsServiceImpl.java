00cf59223d5d17b84146fa48b8eda9949c4e1f61
luchoc <luiscarlin@gmail.com>
2016-12-29 08:18:17 -0500

    transfer ftl to thymeleaf

--- src/main/java/com/ecommerce/service/UserDetailsServiceImpl.java
+++ src/main/java/com/ecommerce/service/UserDetailsServiceImpl.java
@@ -3,6 +3,8 @@ package com.ecommerce.service;
 import com.ecommerce.domain.Role;
 import com.ecommerce.domain.User;
 import com.ecommerce.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -17,15 +19,28 @@ import java.util.Set;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserDetailsServiceImpl.class);
+
     @Autowired
     private UserRepository userRepository;
 
     @Override
     @Transactional(readOnly = true)
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(username);
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 
-        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+        LOGGER.debug("Authenticating user with email={}", email);
+
+        User user = userRepository.findByEmail(email);
+
+        if (user == null) {
+            LOGGER.debug("User with email={} was not found", email);
+            throw new UsernameNotFoundException(String.format("User with email=%s was not found", email));
+        }
+
+        LOGGER.debug("Building a new userdetails User for user with email={}", email);
+
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<GrantedAuthority>();
         for (Role role : user.getRoles()) {
             grantedAuthorities.add(new SimpleGrantedAuthority(role.getName()));
         }
