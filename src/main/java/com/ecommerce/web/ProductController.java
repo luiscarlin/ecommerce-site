55ea076c276c53456ddbbb934fbe0bdbd104511a
luchoc <luiscarlin@gmail.com>
2017-01-05 05:37:30 -0500

    clean up

--- src/main/java/com/ecommerce/web/ProductController.java
+++ src/main/java/com/ecommerce/web/ProductController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.lang.reflect.InvocationTargetException;
-import java.security.Principal;
 
 @Controller
 @RequestMapping("/dashboard/products")
