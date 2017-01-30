bb42d129a8905f4d60dce8ae58466c30ac2307df
Luis Carlin <luiscarlin@gmail.com>
2017-01-29 21:16:31 -0500

    missed csrf token for ajax

--- src/main/resources/static/js/product.js
+++ src/main/resources/static/js/product.js
@@ -2,6 +2,7 @@ $(function() {
   // set csrf token for all ajax requests
   var token = $("input[name='_csrf']").val();
   var header = "X-CSRF-TOKEN";
+
   $(document).ajaxSend(function(e, xhr, options) {
     xhr.setRequestHeader(header, token);
   });
