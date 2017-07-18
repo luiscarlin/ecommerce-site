00b5538a9aa5294b5b9cff2d43be1541ddd2ba5b
Luis Carlin <luiscarlin@gmail.com>
2017-07-17 23:30:07 -0400

    Some cleanup

--- src/main/java/com/ecommerce/repository/UserRepository.java
+++ src/main/java/com/ecommerce/repository/UserRepository.java
@@ -3,8 +3,6 @@ package com.ecommerce.repository;
 import com.ecommerce.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
 }
